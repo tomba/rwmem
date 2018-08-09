30fa7611d5ba6067112b238a08af858b0e61c6c3
Tomi Valkeinen <tomi.valkeinen@ti.com>
2018-08-09 14:29:44 +0300

    example changes

--- examples/detect.sh
+++ examples/detect.sh
@@ -6,3 +6,22 @@ if grep -sqi dra7 $c ; then
 	echo dra7
 fi
 
+if grep -sqi omap5 $c ; then
+	echo omap5
+fi
+
+if grep -sqi omap3 $c ; then
+	echo omap3
+fi
+
+if grep -sqi am437x $c ; then
+	echo am4
+fi
+
+if grep -sqi k2g $c ; then
+        echo k2g
+fi
+
+if grep -sqi ti,am6 $c ; then
+        echo am6
+fi
