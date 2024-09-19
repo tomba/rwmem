fef82b7b4532e392caa2070bfbbd639971ba04ef
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2024-09-19 13:37:54 +0300

    py: update test-i2c

--- py/utils/test-i2c.py
+++ py/utils/test-i2c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import pyrwmem as rw
+import rwmem as rw
 
 # Read EDID one byte at a time from the monitor at i2c-adapter 12
 
@@ -9,8 +9,7 @@ i2c_addr = 0x50
 i2c_addr_len = 1
 i2c_data_len = 1
 
-target = rw.I2CTarget(i2c_adapter, i2c_addr)
-map = target.map(0, 0xff, rw.Endianness.Big, i2c_addr_len, rw.Endianness.Big, i2c_data_len)
+target = rw.I2CTarget(i2c_adapter, i2c_addr, 0, 0x100, rw.Endianness.Big, i2c_addr_len, rw.Endianness.Big, i2c_data_len)
 
 for i in range(256):
     v = target.read(i, 1)
