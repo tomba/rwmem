a040c8dc42e0c8a1f4792da75c0662031aa286a7
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2025-07-20 21:45:26 +0300

    tests: Update test_rwmem_cmd to use the new data files

--- tests/test_rwmem_cmd.py
+++ tests/test_rwmem_cmd.py
@@ -8,8 +8,8 @@ import tempfile
 import unittest
 
 RWMEM_CMD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/../build/rwmem/rwmem'
-DATA_BIN_PATH = os.path.dirname(os.path.abspath(__file__)) + '/data.bin'
-TEST_REGDB_PATH = os.path.dirname(os.path.abspath(__file__)) + '/data.regdb'
+DATA_BIN_PATH = os.path.dirname(os.path.abspath(__file__)) + '/test.bin'
+TEST_REGDB_PATH = os.path.dirname(os.path.abspath(__file__)) + '/test.regdb'
 
 class RwmemTestBase(unittest.TestCase):
     def setUp(self):
@@ -39,53 +39,53 @@ class RwmemNumericReadTests(RwmemTestBase):
 
     def test_numeric_reads_single(self):
         self.assertOutput(['0'],
-                          '0x00 = 0x0f7216c2\n')
+                          '0x00 = 0x7d8c0c39\n')
 
         self.assertOutput(['0x10'],
-                          '0x10 (+0x0) = 0xfbf9f1c4\n')
+                          '0x10 (+0x0) = 0x8ee570d6\n')
 
         self.assertOutput(['-s8', '0'],
-                          '0x00 = 0xc2\n')
+                          '0x00 = 0x39\n')
 
         self.assertOutput(['-s16', '0'],
-                          '0x00 = 0x16c2\n')
+                          '0x00 = 0x0c39\n')
 
         self.assertOutput(['-s16', '0'],
-                          '0x00 = 0x16c2\n')
+                          '0x00 = 0x0c39\n')
 
         self.assertOutput(['-s32', '0'],
-                          '0x00 = 0x0f7216c2\n')
+                          '0x00 = 0x7d8c0c39\n')
 
         self.assertOutput(['-s24', '0'],
-                          '0x00 = 0x7216c2\n')
+                          '0x00 = 0x8c0c39\n')
 
         self.assertOutput(['-s24be', '0'],
-                          '0x00 = 0xc21672\n')
+                          '0x00 = 0x390c8c\n')
 
     def test_numeric_reads_ranges(self):
         self.assertOutput(['0x0-0x10'],
-                          '0x00 = 0x0f7216c2\n' +
-                          '0x04 = 0x7d12d37b\n' +
-                          '0x08 = 0x5e1721ec\n' +
-                          '0x0c = 0x3b0eb509\n')
+                          '0x00 = 0x7d8c0c39\n' +
+                          '0x04 = 0x2c344772\n' +
+                          '0x08 = 0x2f0f10d8\n' +
+                          '0x0c = 0x650d776f\n')
 
         self.assertOutput(['0x0+0x10'],
-                          '0x00 = 0x0f7216c2\n' +
-                          '0x04 = 0x7d12d37b\n' +
-                          '0x08 = 0x5e1721ec\n' +
-                          '0x0c = 0x3b0eb509\n')
+                          '0x00 = 0x7d8c0c39\n' +
+                          '0x04 = 0x2c344772\n' +
+                          '0x08 = 0x2f0f10d8\n' +
+                          '0x0c = 0x650d776f\n')
 
         self.assertOutput(['0x10-0x20'],
-                           '0x10 (+0x0) = 0xfbf9f1c4\n' +
-                           '0x14 (+0x4) = 0x85f114a9\n' +
-                           '0x18 (+0x8) = 0xa4edffdd\n' +
-                           '0x1c (+0xc) = 0x2ecde8ff\n')
+                           '0x10 (+0x0) = 0x8ee570d6\n' +
+                           '0x14 (+0x4) = 0xaed85103\n' +
+                           '0x18 (+0x8) = 0xac6e4f8e\n' +
+                           '0x1c (+0xc) = 0x31c22f34\n')
 
         self.assertOutput(['0x10+0x10'],
-                           '0x10 (+0x0) = 0xfbf9f1c4\n' +
-                           '0x14 (+0x4) = 0x85f114a9\n' +
-                           '0x18 (+0x8) = 0xa4edffdd\n' +
-                           '0x1c (+0xc) = 0x2ecde8ff\n')
+                           '0x10 (+0x0) = 0x8ee570d6\n' +
+                           '0x14 (+0x4) = 0xaed85103\n' +
+                           '0x18 (+0x8) = 0xac6e4f8e\n' +
+                           '0x1c (+0xc) = 0x31c22f34\n')
 
 
 class RwmemNumericWriteTests(RwmemTestBase):
@@ -102,20 +102,20 @@ class RwmemNumericWriteTests(RwmemTestBase):
 
     def test_numeric_writes_single(self):
         self.assertOutput(['0x0=0'],
-                          '0x00 = 0x0f7216c2 := 0x00000000 -> 0x00000000\n')
+                          '0x00 = 0x7d8c0c39 := 0x00000000 -> 0x00000000\n')
 
         self.assertOutput(['0xa0=0'],
-                          '0xa0 (+0x0) = 0xc4f32790 := 0x00000000 -> 0x00000000\n')
+                          '0xa0 (+0x0) = 0x24a91022 := 0x00000000 -> 0x00000000\n')
 
         self.assertOutput(['-s24', '0xb0=0x123456'],
-                          '0xb0 (+0x0) = 0x62ce25 := 0x123456 -> 0x123456\n')
+                          '0xb0 (+0x0) = 0xd8b5dc := 0x123456 -> 0x123456\n')
 
     def test_numeric_writes_ranges(self):
         self.assertOutput(['0x10+0x10=0x1234'],
-                           '0x10 (+0x0) = 0xfbf9f1c4 := 0x00001234 -> 0x00001234\n' +
-                           '0x14 (+0x4) = 0x85f114a9 := 0x00001234 -> 0x00001234\n' +
-                           '0x18 (+0x8) = 0xa4edffdd := 0x00001234 -> 0x00001234\n' +
-                           '0x1c (+0xc) = 0x2ecde8ff := 0x00001234 -> 0x00001234\n')
+                           '0x10 (+0x0) = 0x8ee570d6 := 0x00001234 -> 0x00001234\n' +
+                           '0x14 (+0x4) = 0xaed85103 := 0x00001234 -> 0x00001234\n' +
+                           '0x18 (+0x8) = 0xac6e4f8e := 0x00001234 -> 0x00001234\n' +
+                           '0x1c (+0xc) = 0x31c22f34 := 0x00001234 -> 0x00001234\n')
 
 
 class RwmemRegisterDatabaseTests(RwmemTestBase):
@@ -130,68 +130,60 @@ class RwmemRegisterDatabaseTests(RwmemTestBase):
                            capture_output=True, encoding='ASCII', check=False)
 
         self.assertEqual(res.returncode, 0, res)
-        self.assertIn('TestChip: total 5/11/24', res.stdout)
-        self.assertIn('ByteRegs:', res.stdout)
-        self.assertIn('WordRegs:', res.stdout)
-        self.assertIn('DwordRegs:', res.stdout)
-        self.assertIn('QwordRegs:', res.stdout)
+        self.assertIn('TEST_V3: total 3/14/24', res.stdout)
+        self.assertIn('SENSOR_A:', res.stdout)
+        self.assertIn('SENSOR_B:', res.stdout)
+        self.assertIn('MEMORY_CTRL:', res.stdout)
 
     def test_regdb_byte_register(self):
         # Test 8-bit register access
-        self.assertOutput(['ByteRegs.BYTE0'],
-                          'ByteRegs.BYTE0                 0x00000000 = 0xc2\n' +
-                          '  VAL                             7:0  = 0xc2 \n')
+        self.assertOutput(['SENSOR_A.STATUS_REG'],
+                          'SENSOR_A.STATUS_REG            0x00 = 0x7d8c0c39\n' +
+                          '  MODE                            7:3  = 0x00000007 \n' +
+                          '  ERROR                           2:1  = 0x00000000 \n' +
+                          '  READY                             0  = 0x00000001 \n')
 
-        self.assertOutput(['ByteRegs.BYTE1'],
-                          'ByteRegs.BYTE1                 0x00000001 = 0x16\n' +
-                          '  VAL                             7:0  = 0x16 \n')
+        self.assertOutput(['SENSOR_A.CONTROL_REG'],
+                          'SENSOR_A.CONTROL_REG           0x01 = 0x727d8c0c\n' +
+                          '  MODE                            7:3  = 0x00000001 \n' +
+                          '  ERROR                           2:1  = 0x00000002 \n' +
+                          '  ENABLE                            0  = 0x00000000 \n')
 
     def test_regdb_word_register(self):
         # Test 16-bit register access
-        self.assertOutput(['WordRegs.WORD0'],
-                          'WordRegs.WORD0                 0x00000020 (+0x10) = 0x175b\n' +
-                          '  HIGH                           15:8  = 0x0017 \n' +
-                          '  LOW                             7:0  = 0x005b \n')
+        self.assertOutput(['SENSOR_A.DATA_REG'],
+                          'SENSOR_A.DATA_REG              0x02 = 0x47727d8c\n' +
+                          '  VALUE                          15:0  = 0x00007d8c \n')
 
     def test_regdb_dword_register(self):
         # Test 32-bit register access
-        self.assertOutput(['DwordRegs.DWORD0'],
-                          'DwordRegs.DWORD0               0x00000040 (+0x20) = 0x39a3ab9b\n' +
-                          '  BITS31_24                      31:24 = 0x00000039 \n' +
-                          '  BITS23_16                      23:16 = 0x000000a3 \n' +
-                          '  BITS15_8                       15:8  = 0x000000ab \n' +
-                          '  BITS7_0                         7:0  = 0x0000009b \n')
+        self.assertOutput(['SENSOR_A.COUNTER_REG'],
+                          'SENSOR_A.COUNTER_REG           0x08 = 0x2f0f10d8\n' +
+                          '  COUNT                          31:0  = 0x2f0f10d8 \n')
 
     def test_regdb_tbyte_register(self):
-        # Test 24-bit register access
-        self.assertOutput(['TbyteRegs.TBYTE0'],
-                          'TbyteRegs.TBYTE0               0x00000060 (+0x30) = 0x6d36fd\n' +
-                          '  HIGH8                          23:16 = 0x00006d \n' +
-                          '  MID8                           15:8  = 0x000036 \n' +
-                          '  LOW8                            7:0  = 0x0000fd \n')
-
-        self.assertOutput(['TbyteRegs.RGB'],
-                          'TbyteRegs.RGB                  0x00000063 (+0x33) = 0x78a20f\n' +
-                          '  RED                            23:16 = 0x000078 \n' +
-                          '  GREEN                          15:8  = 0x0000a2 \n' +
-                          '  BLUE                            7:0  = 0x00000f \n')
+        # Test 24-bit register access (3-byte)
+        self.assertOutput(['SENSOR_A.CONFIG_REG'],
+                          'SENSOR_A.CONFIG_REG            0x04 = 0x2c344772\n' +
+                          '  THRESHOLD                      23:16 = 0x00000034 \n' +
+                          '  GAIN                           15:8  = 0x00000047 \n' +
+                          '  OFFSET                          7:0  = 0x00000072 \n')
 
     def test_regdb_qword_register(self):
         # Test 64-bit register access
-        self.assertOutput(['QwordRegs.QWORD0'],
-                          'QwordRegs.QWORD0               0x00000080 (+0x40) = 0x195626529f41411b\n' +
-                          '  HIGH32                         63:32 = 0x0000000019562652 \n' +
-                          '  LOW32                          31:0  = 0x000000009f41411b \n')
+        self.assertOutput(['SENSOR_A.MAX_REG'],
+                          'SENSOR_A.MAX_REG               0x24 = 0x28c13feb\n' +
+                          '  MAX_DATA                       63:0  = 0x28c13feb \n')
 
     def test_regdb_field_access(self):
         # Test specific field access
-        self.assertOutput(['DwordRegs.CONTROL:VALUE'],
-                          'DwordRegs.CONTROL              0x00000044 (+0x24) = 0x0038e625\n' +
-                          '  VALUE                          23:0  = 0x0038e625 \n')
+        self.assertOutput(['SENSOR_A.CONFIG_REG:THRESHOLD'],
+                          'SENSOR_A.CONFIG_REG            0x04 = 0x2c344772\n' +
+                          '  THRESHOLD                      23:16 = 0x00000034 \n')
 
-        self.assertOutput(['DwordRegs.CONTROL:ENABLE'],
-                          'DwordRegs.CONTROL              0x00000044 (+0x24) = 0x0038e625\n' +
-                          '  ENABLE                            31 = 0x00000000 \n')
+        self.assertOutput(['SENSOR_A.STATUS_REG:MODE'],
+                          'SENSOR_A.STATUS_REG            0x00 = 0x7d8c0c39\n' +
+                          '  MODE                            7:3  = 0x00000007 \n')
 
 
 if __name__ == '__main__':
