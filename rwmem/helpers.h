0dd20eb05c2f379fbd9b71c7b05f37d55a3a2125
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2025-12-12 10:15:48 +0200

    Remove ERR_ON_ERRNO

--- rwmem/helpers.h
+++ rwmem/helpers.h
@@ -11,8 +11,6 @@
 
 void err_vprint(std::string_view fmt, std::format_args args);
 
-void errno_vprint(int eno, std::string_view fmt, std::format_args args);
-
 template<typename... Args>
 void print(std::format_string<Args...> format_str, Args&&... args)
 {
@@ -39,17 +37,6 @@ void ERR_ON(bool condition, std::format_string<Args...> format_str, Args&&... ar
 	if (unlikely(condition)) {
 		const auto& vargs = std::make_format_args(args...);
 		err_vprint(format_str.get(), vargs);
-		exit(1);
-	}
-}
-
-template<typename... Args>
-void ERR_ON_ERRNO(bool condition, std::format_string<Args...> format_str, Args&&... args)
-{
-	if (unlikely(condition)) {
-		int eno = errno;
-		const auto& vargs = std::make_format_args(args...);
-		errno_vprint(eno, format_str.get(), vargs);
 		exit(1);
 	}
 }
